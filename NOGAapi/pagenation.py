2fb01578d25f6b9a2d486e13cf6a86b469c59210
Oudisho-Qattyne <oudiqa2003@gmail.com>
2024-08-12 01:31:09 +0300

    change page size to 10

--- NOGAapi/pagenation.py
+++ NOGAapi/pagenation.py
@@ -4,4 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 class Paginator(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 30
+    max_page_size = 40
