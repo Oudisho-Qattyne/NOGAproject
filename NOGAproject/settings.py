e5368a8e86311754f852f86294c52849b11eb514
Oudisho-Qattyne <oudiqa2003@gmail.com>
2024-08-15 00:26:49 +0300

    fixed bug in quantit in request branch

--- NOGAproject/settings.py
+++ NOGAproject/settings.py
@@ -91,16 +91,16 @@ WSGI_APPLICATION = 'NOGAproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
